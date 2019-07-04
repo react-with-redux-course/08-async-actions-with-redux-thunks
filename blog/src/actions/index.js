668e3825b53ef75114da24ce17cee503e13ca2e3
Grace Lee <igracelee1@gmail.com>
2019-07-04 00:32:09 -0400

    thunk for fetching posts

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -13,10 +13,22 @@ export const fetchPosts = async () => {
 };
 */
 
-export const fetchPosts = async () => {
+// typical format of redux thunks
+export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
-    return {
-        type: 'FETCH_POSTS',
-        payload: response
-    };
+    dispatch({ type: 'FETCH_POSTS', payload: response });
 };
+
+// alternative way of writing above thunk
+
+// export const fetchPosts = () => {
+//     /*
+//         removed the second argument of 'getState'
+//         if we're not making use of it
+//     */
+//     return async (dispatch) => {
+//         const response = await jsonPlaceholder.get('/posts');
+
+//         dispatch({ type: 'FETCH_POSTS', payload: response })
+//     };
+// };
