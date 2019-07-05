4e81e01e3b011d88f93532f2b4680da9aa9bce3b
Grace Lee <igracelee1@gmail.com>
2019-07-04 22:11:18 -0400

    switch statements in postsReducer

--- blog/src/reducers/postsReducer.js
+++ blog/src/reducers/postsReducer.js
@@ -4,6 +4,17 @@
     our reducer
 */
 
-export default (state, action) => {
-    return 123;
+/* 
+    returnig a state of an empty array the first
+    time the recuducer is called
+*/
+export default (state = [], action) => {
+    switch (action.type) {
+        // will typically have more cases than 1 in other apps...
+        case 'FETCH_POSTS':
+            return action.payload;
+        default:
+            // never want to return 'undefined' from a reducer
+            return state;
+    }
 };
