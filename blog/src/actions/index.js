763d3e79ba542e1acece95b3f6b7b6a79d1bbdcb
Grace Lee <igracelee1@gmail.com>
2019-07-06 23:34:19 -0400

    UserHeader component

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -32,3 +32,9 @@ export const fetchPosts = () => async dispatch => {
 //         dispatch({ type: 'FETCH_POSTS', payload: response })
 //     };
 // };
+
+// using id of the user we want to fetch
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data});
+};
--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
     /*
@@ -21,6 +22,8 @@ class PostList extends React.Component {
                             <h2>{post.title}</h2>
                             <p>{post.body}</p>
                         </div>
+                        {/* if we go back to the post endpoint, we have a userId property on each post */}
+                        <UserHeader userId={post.userId} />
                     </div>
                 </div>
             );
